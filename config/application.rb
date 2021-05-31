13a3a08e274e468e5d32bb80f1cfc6432c3e3f77
ryouhei1222 <dvdfbfd@gmail.com>
2021-05-31 18:19:47 +0900

    投稿時刻の日本時間への修正

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,8 @@ Bundler.require(*Rails.groups)
 
 module ChatApp
   class Application < Rails::Application
+    config.i18n.default_locale = :ja
+    config.time_zone = 'Tokyo'
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
     config.generators do |g|
